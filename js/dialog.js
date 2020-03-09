ac82f7b89cb9fe776be33d43069675e95df31822
Gozh78 <54843808+Gozh78@users.noreply.github.com>
2020-03-09 20:30:48 +0300

    17. Учебный проект: фильтруй заклинания!

--- js/dialog.js
+++ js/dialog.js
@@ -3,19 +3,12 @@
 // у блока setup. Нажатие на элемент .setup-close, расположенный
 // внутри блока setup возвращает ему класс hidden.
 
-// var setup = document.querySelector('.setup');
 (function () {
   var setup = document.querySelector('.setup');
   var setupOpen = document.querySelector('.setup-open');
   var setupClose = setup.querySelector('.setup-close');
   var userNameInput = setup.querySelector('.setup-user-name');
-  var setupPlayer = setup.querySelector('.setup-player');
-  var setupWizardCoatColor = setupPlayer.querySelector('.wizard-coat');
-  var setupWizardEyesColor = setupPlayer.querySelector('.wizard-eyes');
-  var setupFireballColor = setupPlayer.querySelector('.setup-fireball-wrap');
-  var setupCoatInput = setupPlayer.querySelector('input:first-of-type');
-  var setupEyesInput = setupPlayer.querySelector('input:last-of-type');
-  var setupFireballInput = setupFireballColor.querySelector('input');
+
   var popupCoordinateY = window.getComputedStyle(setup).top;
   var popupCoordinateX = window.getComputedStyle(setup).left;
 
@@ -74,74 +67,4 @@
       userNameInput.setCustomValidity('');
     }
   });
-
-  var onWizardElementClick = function (element, array, input, counter) {
-    element.style.fill = array[counter];
-    input.value = array[counter];
-  };
-  /* Хотел заменить куски кода со счётчиком на функцию, но счётчик некорректно работает
-  var countWizardElements = function (arrayColors) {
-    window.setup.allCounter += 1;
-    if (window.setup.allCounter === window.setup.arrayColors.length) {
-      window.setup.allCounter = window.setup.FIRST_ARRAY_ELEMENT_NUMBER;
-    }
-  };
-  */
-  setupWizardCoatColor.addEventListener('click', function () {
-    onWizardElementClick(setupWizardCoatColor, window.setup.WIZARD_COAT_COLORS, setupCoatInput, window.setup.allCounter);
-    window.setup.allCounter += 1;
-    if (window.setup.allCounter === window.setup.WIZARD_COAT_COLORS.length) {
-      window.setup.allCounter = window.setup.FIRST_ARRAY_ELEMENT_NUMBER;
-    }
-    // countWizardElements(window.setup.WIZARD_COAT_COLORS);
-  });
-
-  setupWizardEyesColor.addEventListener('click', function () {
-    onWizardElementClick(setupWizardEyesColor, window.setup.WIZARD_EYES_COLORS, setupEyesInput, window.setup.allCounter);
-    window.setup.allCounter += 1;
-    if (window.setup.allCounter === window.setup.WIZARD_EYES_COLORS.length) {
-      window.setup.allCounter = window.setup.FIRST_ARRAY_ELEMENT_NUMBER;
-    }
-    // countWizardElements(window.setup.WIZARD_EYES_COLORS);
-  });
-
-  setupFireballColor.addEventListener('click', function () {
-    setupFireballColor.style.backgroundColor = window.setup.WIZARD_FIREBALL_COLORS[window.setup.allCounter];
-    onWizardElementClick(setupFireballColor, window.setup.WIZARD_FIREBALL_COLORS, setupFireballInput, window.setup.allCounter);
-    window.setup.allCounter += 1;
-    if (window.setup.allCounter === window.setup.WIZARD_FIREBALL_COLORS.length) {
-      window.setup.allCounter = window.setup.FIRST_ARRAY_ELEMENT_NUMBER;
-    }
-    // countWizardElements(window.setup.WIZARD_FIREBALL_COLORS);
-  });
-
-
-  /*
-  setupWizardCoatColor.addEventListener('click', function () {
-    onWizardElementClick(setupWizardCoatColor, WIZARD_COAT_COLORS, setupCoatInput, coatCounter);
-    coatCounter += 1;
-    if (coatCounter === WIZARD_COAT_COLORS.length) {
-      coatCounter = FIRST_ARRAY_ELEMENT_NUMBER;
-    }
-  });
-
-  setupWizardEyesColor.addEventListener('click', function () {
-
-    onWizardElementClick(setupWizardEyesColor, WIZARD_EYES_COLORS, setupEyesInput, eyesCounter);
-    eyesCounter += 1;
-    if (eyesCounter === WIZARD_EYES_COLORS.length) {
-      eyesCounter = FIRST_ARRAY_ELEMENT_NUMBER;
-    }
-  });
-
-  setupFireballColor.addEventListener('click', function () {
-
-    setupFireballColor.style.backgroundColor = WIZARD_FIREBALL_COLORS[fireballCounter];
-    onWizardElementClick(setupFireballColor, WIZARD_FIREBALL_COLORS, setupFireballInput, fireballCounter);
-    fireballCounter += 1;
-    if (fireballCounter === WIZARD_FIREBALL_COLORS.length) {
-      fireballCounter = FIRST_ARRAY_ELEMENT_NUMBER;
-    }
-  });
-  */
 })();
